8a641afbd3115c7abe1b01d97f46c0ba0210d0d6
Ejjay <ejalloso021@gmail.com>
2024-10-27 18:21:12 +0800

    Update Chat.tsx

--- src/app/(main)/messages/Chat.tsx
+++ src/app/(main)/messages/Chat.tsx
@@ -1,26 +1,30 @@
-"use client";
+"use client"; // Add this line at the top
 
 import { Loader2 } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Chat as StreamChat } from "stream-chat-react";
 import ChatChannel from "./ChatChannel";
 import ChatSidebar from "./ChatSidebar";
 import useInitializeChatClient from "./useInitializeChatClient";
 
-export default function Chat() {
+export default function Chat({ setShowTopBar }: { setShowTopBar: (show: boolean) => void; }) {
   const chatClient = useInitializeChatClient();
-
   const { resolvedTheme } = useTheme();
-
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    // Hide the top bars when in chat
+    setShowTopBar(false);
+    return () => setShowTopBar(true); // Show it back when leaving
+  }, [setShowTopBar]);
 
   if (!chatClient) {
     return <Loader2 className="mx-auto my-3 animate-spin" />;
   }
 
   return (
-    <main className="relative w-full overflow-hidden rounded-2xl bg-card shadow-sm">
+    <main className="relative w-full h-screen overflow-hidden rounded-2xl bg-card shadow-sm"> {/* Fullscreen */}
       <div className="absolute bottom-0 top-0 flex w-full">
         <StreamChat
           client={chatClient}
