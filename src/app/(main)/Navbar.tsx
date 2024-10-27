f7bad8372f47a4114631196210998be8f1bb3578
Ejjay <ejalloso021@gmail.com>
2024-10-27 17:06:59 +0800

    Update Navbar.tsx

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 px-5 py-3"> {/* Adjust spacing and alignment */}
         <Link href="/" className="text-2xl font-bold text-primary">
-          bugbook
+          pcmi
         </Link>
         <div className="flex-grow"> {/* Allow search field to grow */}
           <SearchField /> {/* Keep SearchField inside a container to control width */}
