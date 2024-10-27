319129be3adab8a5ad41fc34bc9308fe1c093f36
Ejjay <ejalloso021@gmail.com>
2024-10-27 19:18:11 +0800

    Update page.tsx

--- src/app/(main)/messages/page.tsx
+++ src/app/(main)/messages/page.tsx
@@ -1,16 +1,10 @@
-"use client"; // This directive allows the use of hooks
-
+import { Metadata } from "next";
 import Chat from "./Chat";
-import { useState } from "react";
-import { metadata } from "./metadata"; // Import the metadata
+
+export const metadata: Metadata = {
+  title: "Messages",
+};
 
 export default function Page() {
-  const [showTopBar, setShowTopBar] = useState(true); // State to control visibility
-
-  return (
-    <>
-      {/* Remove the TopBar reference since it doesn't exist */}
-      <Chat setShowTopBar={setShowTopBar} />
-    </>
-  );
+  return <Chat />;
 }
