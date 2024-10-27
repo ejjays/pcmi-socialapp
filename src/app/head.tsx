a7d19aa7961e09e75aa61e0befe4e369107bf071
Ejjay <ejalloso021@gmail.com>
2024-10-27 23:45:48 +0800

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | 𝗽𝗰𝗺𝗶",
+    template: "PCMI | %s",
     default: "𝗽𝗰𝗺𝗶",
   },
   description: "Pag-ibig Christian Ministries Infanta Quezon : Official App, Designed for Youths",
