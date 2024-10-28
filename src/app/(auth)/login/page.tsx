ee38e196ec956e1ad918ef38d6473d186a77add8
Ejjay <ejalloso021@gmail.com>
2024-10-29 03:07:57 +0800

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import loginImage from "@/assets/login-image.jpg";
+import loginImage from "@/assets/login-pcmi.jpg";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
