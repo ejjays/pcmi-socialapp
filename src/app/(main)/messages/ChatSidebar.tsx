1c193f9b24f6e5fe7adc3fde52176fe0787afbba
Ejjay <ejalloso021@gmail.com>
2024-10-28 19:35:05 +0800

    Update ChatSidebar.tsx

--- src/app/(main)/messages/ChatSidebar.tsx
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -61,7 +61,7 @@ export default function ChatSidebar({ open, onClose }: ChatSidebarProps) {
     <div
       className={cn(
         "size-full flex-col border-e",
-        isMobile ? (open ? "flex" : "hidden") : "md:flex md:w-72"
+        isMobile ? (open ? "flex" : "hidden") : (open ? "flex md:w-72" : "hidden")
       )}
       style={{ zIndex: 10 }}
     >
