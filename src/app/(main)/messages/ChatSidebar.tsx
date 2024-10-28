d671652fb046f5de5c31a1acf695874f3eb241e3
Ejjay <ejalloso021@gmail.com>
2024-10-28 19:07:38 +0800

    Update ChatSidebar.tsx

--- src/app/(main)/messages/ChatSidebar.tsx
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -58,13 +58,7 @@ export default function ChatSidebar({ open, onClose }: ChatSidebarProps) {
   console.log(`Rendering Sidebar: isMobile: ${isMobile}, open: ${open}`);
 
   return (
-    <div
-      className={cn(
-        "size-full flex-col border-e",
-        isMobile ? (open ? "flex" : "hidden") : "md:flex md:w-72"
-      )}
-      style={{ zIndex: 10 }} // Ensure it appears on top
-    >
+    <div className="size-full flex-col border-e flex md:w-72" style={{ zIndex: 10 }}>
       <MenuHeader onClose={onClose} />
       <ChannelList
         filters={{
