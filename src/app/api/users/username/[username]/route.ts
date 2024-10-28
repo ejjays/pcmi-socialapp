9f69b7efe6393ea28dcbb71d84ad4eb86f026b66
Ejjay <ejalloso021@gmail.com>
2024-10-29 01:12:32 +0800

    Update route.ts

--- src/app/api/users/username/[username]/route.ts
+++ src/app/api/users/username/[username]/route.ts
@@ -4,7 +4,7 @@ import { getUserDataSelect } from "@/lib/types";
 
 export async function GET(
   req: Request,
-  { params: { username } }: { params: { username: string } }
+  { params: { username } }: { params: { username: string } },
 ) {
   try {
     const { user: loggedInUser } = await validateRequest();
@@ -31,49 +31,5 @@ export async function GET(
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
-  }
-}
-
-export async function POST(
-  req: Request,
-  { params: { username } }: { params: { username: string } }
-) {
-  try {
-    const { user: loggedInUser } = await validateRequest();
-
-    if (!loggedInUser || !loggedInUser.isAdmin) {
-      return Response.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
-    const { userId } = await req.json();
-
-    // Check if the user exists
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (!user) {
-      return Response.json({ error: "User not found" }, { status: 404 });
-    }
-
-    // Update the user's isVerified field using a raw SQL query
-    await prisma.$queryRaw`
-      UPDATE "users"
-      SET "isVerified" = true
-      WHERE "id" = ${userId}
-    `;
-
-    const updatedUser = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    return Response.json(updatedUser);
-  } catch (error) {
-    console.error("Error updating user:", error);
-    return Response.json({ error: "Error updating user" }, { status: 500 });
   }
 }
